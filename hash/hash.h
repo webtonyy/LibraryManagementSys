cfcfd67bfec36815f83dd0e15a639a1c5669a591
Gabriel-VFernandes <gabrielvf2001@gmail.com>
2024-12-16 22:13:13 -0300

    hash

--- hash/hash.h
+++ hash/hash.h
@@ -1,30 +1,34 @@
 #ifndef HASH_TABLE_OPEN_H
 #define HASH_TABLE_OPEN_H
-#define TABLE_SIZE 50
+#define TABLE_SIZE 100
+#define MAX_GEN 20
 
 
-typedef struct Livro{
+typedef struct {
     int id;
     int chave;
+    char titulo[TABLE_SIZE];
     char autor[TABLE_SIZE]; 
     char genero[TABLE_SIZE];
-} Livro;
+} LivroH;
 
-typedef struct HashTable {
+typedef struct {
     Livro *autor[TABLE_SIZE];
-    Livro *genero[TABLE_SIZE];
+    char *genero[MAX_GEN];
+    int num_gen;
 } HashTable;
 
 
 int funcaohash_string(const char *nome);
 int funcaohash(int chave);
 void table_init(HashTable *t);
-Livro autores();
-Livro generos();
-void inserir_livro(HashTable *t, const char *nome);
-Livro* busca_livro(HashTable *t, const char *nome);
-void imprimir(HashTable *t);
-void deletar(HashTable *t, const char *nome);
+void genero_possivel(HashTable *t);
+int genero_valido(HashTable *t, const char *genero);
+void inserir_autor(HashTable *t, const char *nome);
+void inserir_genero(HashTable *t, const char *nome) 
+void print_autores(HashTable *t);
+void print_generos(HashTable *t);
+void deletar_autor(HashTable *t, const char *nome);
 void hash_free(HashTable *t);
 
 #endif 
