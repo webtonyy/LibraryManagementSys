e2e52e1840bd40659070d448aafc7022fdc12109
Gabriel-VFernandes <gabrielvf2001@gmail.com>
2024-12-19 21:38:01 -0300

    catalogo

--- include/catalogo.h
+++ include/catalogo.h
@@ -36,21 +36,49 @@ typedef struct catalogo {
     int nl;                 // Número de livros no catálogo
 } Catalogo;
 
-// Funções principais do catálogo
+// Função para inicializar um livro
 Livro* livro_init(const char *nome, const char *genero, const char *autor);
+
+// Função para inicializar um catálogo
 Catalogo* catalogo_init();
+
+// Função para editar informações de um livro no catálogo
 void editar_livro(Catalogo *c, const char *nome_errado, const char *novo_nome, const char *autor,const char *genero);
+
+// Função para adicionar um livro ao catálogo
 Livro* add_livro(Catalogo *c, Livro *l);
+
+// Função para remover um livro do catálogo
 void remover_livro(Catalogo *c, const char *nome, const char *autor);
+
+// Função para listar todos os livros no catálogo
 void listar_livros(Catalogo *c);
-int devolve_livro(Catalogo *c, Livro *l); // Retorna 1 se devolvido, 2 se doado
-int emprestar_livro(Catalogo *c, Livro *l); // Retorna 1 se alugado, 0 se indisponível, -1 em caso de erro
-int buscar_por_nome(Catalogo *c, const char *nome); // Busca todos os livros com o mesmo título
+
+// Função para devolver um livro. Retorna 1 se devolvido, 2 se doado
+int devolve_livro(Catalogo *c, Livro *l);
+
+// Função para emprestar um livro. Retorna 1 se alugado, 0 se indisponível, -1 em caso de erro
+int emprestar_livro(Catalogo *c, Livro *l);
+
+// Função para buscar livros por nome. Busca todos os livros com o mesmo título
+int buscar_por_nome(Catalogo *c, const char *nome);
+
+// Função para verificar o status de um livro específico
 void verificar_status(Catalogo *c, const char *nome, const char *autor);
+
+// Função para liberar a memória alocada para o catálogo
 void free_catalogo(Catalogo *c);
+
+// Função para salvar o catálogo em um arquivo
 void salvar_catalogo(Catalogo *c, const char *nome_arquivo);
+
+// Função para carregar um catálogo de um arquivo
 Catalogo* carregar_catalogo(const char *nome_arquivo);
+
+// Função para liberar a memória alocada para um livro
 void free_livro(Livro *livro);
+
+// Função para liberar a memória alocada para um nó da árvore
 void free_no(No *atual);
 
-#endif // _CATALOGO_H
+#endif 
